04783a5cb7f644eb5927b2c55cd567af9f1c5543
Faith <978202378@qq.com>
2016-08-02 19:46:04 +0800

    Initial Commit

---
+++ ObjcSetAssociatedObjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcSetAssociatedObjectDemo
+//
+//  Created by kangshibiao on 16/8/2.
+//  Copyright © 2016年 ZheJiangTianErRuanJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcSetAssociatedObjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcSetAssociatedObjectDemo
+//
+//  Created by kangshibiao on 16/8/2.
+//  Copyright © 2016年 ZheJiangTianErRuanJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
